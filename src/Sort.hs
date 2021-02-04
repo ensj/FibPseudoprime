4a7e8f306c645ef003ff07f6e44410550520c650
smashalkar <smashalkar@scu.edu>
2021-02-04 12:00:25 -0800

    Functioning functions.

--- src/Sort.hs
+++ src/Sort.hs
@@ -1,21 +1,32 @@
 module Sort
     ( cleanList,
-      listProducts,
+      splitByLength,
       subsets
     ) where
 
 import Data.List ( filter, product )
 import Prelude
 
+--Cleans list--
 cleanList :: [Integer] -> ([Integer], [Integer])
 cleanList n = (a, b) where
     a = filter (\n -> (n `mod` 5 == 2) || (n `mod` 5 == 3)) n
     b = filter (\n -> (n `mod` 5 == 1) || (n `mod` 5 == 4)) n
 
-listProducts :: ([Integer], [Integer]) -> [Integer]
-listProducts (n, b) = x where
-    x = filter (\n -> n `mod` 5 == 1) n
 
-subsets :: [Int] -> [[Int]]
+--Calculates all possible subsets--
+subsets :: [Integer] -> [[Integer]]
 subsets []  = [[]]
 subsets (x:xs) = subsets xs ++ map (x:) (subsets xs)
+
+
+--Filters and splits list into singletons and odd products--
+--Remove the 'map product' in each case to see the original lists (useful for debugging)--
+splitByLength :: [[Integer]] -> ([Integer], [Integer])
+splitByLength n = (a, b) where
+    a = map product (filter (\n -> length n == 1) n)
+    b = map product (filter (\n -> (length n > 1) && (length n `mod` 2 == 1)) n)
+
+mapProductDebug :: [[Integer]] -> [Integer]
+mapProductDebug n = m where
+    m = map product n
