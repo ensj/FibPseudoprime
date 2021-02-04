d346d57b00354a84e885e01381b6cc57fa4d04c4
smashalkar <smashalkar@scu.edu>
2021-02-04 14:03:52 -0800

    Final function.

--- src/Sort.hs
+++ src/Sort.hs
@@ -19,7 +19,6 @@ subsets :: [Integer] -> [[Integer]]
 subsets []  = [[]]
 subsets (x:xs) = subsets xs ++ map (x:) (subsets xs)
 
-
 --Filters and splits list into singletons and odd products--
 --Remove the 'map product' in each case to see the original lists (useful for debugging)--
 splitByLength :: [[Integer]] -> ([Integer], [Integer])
@@ -27,6 +26,8 @@ splitByLength n = (a, b) where
     a = map product (filter (\n -> length n == 1) n)
     b = map product (filter (\n -> (length n > 1) && (length n `mod` 2 == 1)) n)
 
-mapProductDebug :: [[Integer]] -> [Integer]
-mapProductDebug n = m where
-    m = map product n
+--Pass all the odd products to this function, along with (map product (even factor subsets))--
+cartesianProduct :: ([Integer], [Integer]) -> [Integer]
+cartesianProduct (a, b) = x where
+    x = map product (sequence [a, b])
+
