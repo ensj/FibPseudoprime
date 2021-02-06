0ef33b271281289f604bc7e6ed0251effdf3615b
smashalkar <smashalkar@scu.edu>
2021-02-05 20:47:26 -0800

    Bug fixes, presentation changes.

--- app/Main.hs
+++ app/Main.hs
@@ -29,20 +29,19 @@ fibFactorsCleaned n limit = do
         let primeFactors = pfactors(fibn)
         let ntoi = toInteger(n) -- n to Integer
         let pfFiltered = filter (\factor -> (factor `mod` ntoi == 1) || (factor `mod` ntoi == ntoi - 1)) primeFactors
+        -- print("Filtered factors: " ++ show pfFiltered)
 
         -- clean to ([+/- 1 mod 5], [+/- 2 mod 5])
         let cl = cleanList pfFiltered
-        -- print("cleanList")
-        -- print(cl)
+        -- print("cleanList: " ++ show cl)
 
         -- (map product) +/- 1 mod 5 subsets
-        let oneModFive = map product $ filter (\set -> set /= []) $ subsets $ fst cl
-        -- pair (singleton factors, and odd products)
-        let singletonOddProd = splitByLength $ filter (\set -> set /= []) $ subsets $ snd cl
-        -- print("oneModFive")
-        -- print(oneModFive)
-        -- print("singletonOddProd")
-        -- print(singletonOddProd)
+        let oneModFive = map product $ filter (\set -> set /= []) $ subsets $ snd cl
+        -- print("One mod five: " ++ show oneModFive)
+
+        let singletonOddProd = splitByLength $ filter (\set -> set /= []) $ subsets $ fst cl
+        -- print("Single moms in your area: " ++ show singletonOddProd)
+
 
         let fibpsp = concat [cartesianProduct(oneModFive, fst singletonOddProd), 
                              snd singletonOddProd, 
@@ -51,18 +50,17 @@ fibFactorsCleaned n limit = do
 
         if length fibpsp == 0 
         then do
-            print("No fibpseudoprimes! " ++ show n)
+            print("No candidates from F_{" ++ show n ++ "}.")
             fibFactorsCleaned (n+1) limit
         else do
-            print(show n ++ "th fib number: " ++ show fibn)
-            print "FibPsp:"
+            print("Viable candidates generated from F_{" ++ show n ++ "} (" ++ show fibn ++ "): ")
             print fibpsp
 
             let baseTwoFibpsp = filter (\elem -> isFermatPP elem 2) fibpsp
 
             if length baseTwoFibpsp /= 0
             then do
-                print "Base two pseudoprime psp:"
+                print "Base-2 Pseudoprime found!"
                 print(baseTwoFibpsp)
                 return (-1)
             else fibFactorsCleaned (n+1) limit
