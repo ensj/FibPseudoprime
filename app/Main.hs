9ad9738ea2cd803e9b718a48ddbf3bdb8ee8b7fc
ensj <ensjtrans@gmail.com>
2021-02-14 21:30:37 -0500

    fixed output for schaefer

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,17 @@ module Main where
 import Lib
 import Math.NumberTheory.Factor ( pfactors )
 import Math.NumberTheory.Primes
-import Data.Text (pack)
-import Data.Text.IO ( writeFile )
-import Data.List
+import Data.Text as T
+import Data.Text.IO as T.IO
+import Data.List as L
 
 printFibFactors :: (Int, Integer, [Integer]) -> IO ()
 printFibFactors (n, fibn, factors) = do 
     print(show n ++ "th Fib: " ++ show fibn)
-    print("Factors:  " ++ show factors ++ "  --  length: " ++ show (length factors))
+    print("Factors:  " ++ show factors ++ "  --  length: " ++ show (L.length factors))
 
 main :: IO ()
-main = Data.Text.IO.writeFile "output/output.txt" (pack $ show $ map carlTest [1, 2 .. 300])
+main = T.IO.writeFile "output/output.txt" 
+        (T.map (\c -> if c == '(' then '[' else if c == ')' then ']' else c) $ 
+            T.pack $
+            show $ L.map carlTest [1, 2 .. 300])
